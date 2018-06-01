642d885a1cbe53e3cdb900a569b513079cfa3662
xujun <234115233@qq.com>
2018-06-01 11:36:58 +0800

    画像系统脚本相关

---
+++ 画像系统变量自动化测试/data2.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+data2 = {
+    "server_ip_port":["http://10.0.4.147","8080"],
+	"idcard":"110000000000000015",
+	"mobile":"13000000008",
+	#"siteNo_All":"",
+    "end":"end"
+}
+
+
+
---
+++ 画像系统变量自动化测试/variable_txt.py
@@ -0,0 +1,58 @@
+#coding=utf-8
+
+variable_txt= {
+    "parms":
+	[{"name":"YiX_OverdueM6_Array","scope":1,"value":[3.0,33.0,333.0,3333.0,33333.0,333333.0,3333333.0,3.3333333E7,999.0,101010.0,111.0]},
+	{"name":"YiX_LastY3_OtherCreditNum","scope":1,"value":3.0},
+	{"name":"YiX_OverdueAmount_Array_Left","scope":1,"value":[0.0,5000.0,5000.0,0.0,0.0,0.0,100.0,1000.0,100.0,100.0,0.0]},
+	{"name":"YiX_Apt_NumODM6Plus","scope":1,"value":102009.0},
+	{"name":"YiX_LastM4_LoanRejNum","scope":1,"value":2.0},
+	{"name":"YiX_Apt_NumLoan","scope":1,"value":11.0},
+	{"name":"YiX_Apt_L6MNumOthCorpInq","scope":1,"value":7.0},
+	{"name":"YiX_Apt_NumOthCorp","scope":1,"value":5.0},
+	{"name":"YiX_Apt_L5YTotalODLoanAmt","scope":1,"value":200.0},
+	{"name":"YiX_Last6Months_Num","scope":1,"value":7.0},
+	{"name":"YiX_LastY3_OtherLoanNum","scope":1,"value":3.0},
+	{"name":"yx_overdue_num_90days","scope":1,"value":1.010201009E9},
+	{"name":"YiX_Loan_Num","scope":1,"value":11.0},
+	{"name":"YiX_OverdueM3_Array","scope":1,"value":[5.0,55.0,555.0,5555.0,55555.0,555555.0,5555555.0,5.5555555E7,99999.0,1.01010101E9,111111.0]},
+	{"name":"YiX_LastY6_OtherLoanNum","scope":1,"value":3.0},
+	{"name":"YiX_LastY1_RisksFalseNum","scope":1,"value":1.0},
+	{"name":"YiX_Apt_NumInqCorp","scope":1,"value":0.0},
+	{"name":"YiX_ShareCode","scope":1,"value":200},
+	{"name":"YiX_Apt_YXScore","scope":1,"value":536.0},
+	{"name":"YiX_ApprovalStatus_Array","scope":1,"value":[u"203",u"203",u"204",u"203",u"203",u"203",u"203",u"203",u"302",u"203",u"203"]},
+	{"name":"YiX_LastY1_RisksLoseNum","scope":1,"value":3.0},
+	{"name":"YiX_Apt_L3MNumOthCorpInq","scope":1,"value":6.0},
+	{"name":"YiX_OverdueTotal_Array","scope":1,"value":[2.0,22.0,222.0,2222.0,22222.0,222222.0,2222222.0,2.2222222E7,99.0,1010.0,111.0]},
+	{"name":"YiX_LoanTime_Array","scope":1,"value":[u"201705",u"201705",u"201805",u"201612",u"201708",u"201707",u"201805",u"201805",u"201704",u"201703",u"201702"]},
+	{"name":"YiX_LastM1_LoanRejNum","scope":1,"value":0.0},
+	{"name":"yx_underly_record_num","scope":1,"value":2.0},
+	{"name":"YiX_Apt_PD","scope":1,"value":0.1486},
+	{"name":"YiX_Periods_Array","scope":1,"value":[1.0,6.0,6.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]},
+	{"name":"yx_risk_num","scope":1,"value":6.0},
+	{"name":"YiX_Apt_TotalODLoanAmt","scope":1,"value":200.0},
+	{"name":"YiX_12month_NocusFina_Num","scope":1,"value":3.0},
+	{"name":"YiX_QueryCode","scope":1,"value":200},
+	{"name":"YiX_LastY1_RisksFakeNum","scope":1,"value":1.0},
+	{"name":"YiX_OverdueStatus_Array","scope":1,"value":[u"1",u"11",u"111",u"1111",u"11111",u"111111",u"1111111",u"11111111",u"11111111",u"111111111",u"12331313"]},
+	{"name":"YiX_OverAmount_Array_Right","scope":1,"value":[1000.0,10000.0,10000.0,1000.0,1000.0,1000.0,u" 1000",u" 5000",1000.0,1000.0,1000.0]},
+	{"name":"YiX_LastY1_OtherCreditNum","scope":1,"value":2.0},
+	{"name":"YiX_LastY1_RisksNum","scope":1,"value":1.0},
+	{"name":"YiX_LastM3_LoanRejNum","scope":1,"value":2.0},
+	{"name":"YiX_LoanStatus_Array","scope":1,"value":[u"4",u"44",u"444",u"4444",u"44444",u"444444",u"4444444",u"44444444",u"302",u"302",u"11111"]},
+	{"name":"yx_otherorgan_times","scope":1,"value":5.0},
+	{"name":"YiX_LastY1_OtherLoanNum","scope":1,"value":1.0},
+	{"name":"YiX_Apt_NumOD","scope":1,"value":1109.0},
+	{"name":"YiX_Apt_L5YNumODLoan","scope":1,"value":2.0},
+	{"name":"YiX_LastY6_OtherCreditNum","scope":1,"value":3.0},
+	{"name":"YiX_LoanType_Array","scope":1,"value":[21.0,21.0,21.0,22.0,21.0,23.0,23.0,23.0,21.0,21.0,21.0]},
+	{"name":"YiX_LoanAmount_Array_Right","scope":1,"value":[u"1000",u"10000",u"10000",u"1000",u"1000",u"1000",u" 1000",u" 5000",u"1000",u"1000",u"1000"]},
+	{"name":"YiX_LoanAmount_Array_Left","scope":1,"value":[u"0",u"5000",u"5000",u"0",u"0",u"0",u"0",u"1000",u"100",u"100",u"0"]}]
+}
+
+
+
+
+
+
