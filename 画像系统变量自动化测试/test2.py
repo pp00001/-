82a82abcc40b2be856a3e0879a0eab99ca896508
xujun <234115233@qq.com>
2018-06-21 17:00:38 +0800

    新增支付宝和微信接口

--- 画像系统变量自动化测试/test2.py
+++ 画像系统变量自动化测试/test2.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 def gettab(key):
-    if isinstance(key,float) or isinstance(key,int) or isinstance(key,list):
+    if isinstance(key,float) or isinstance(key,int) or isinstance(key,list) or isinstance(key,long):
         return key
     else:
         return key.decode('utf-8')
